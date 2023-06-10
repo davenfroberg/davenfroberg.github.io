2d104fb3c1176eaf3407e6c9e2a597d850ebae4e
davenfroberg <davenfroberg@gmail.com>
2023-06-10 16:01:54 -0600

    Started work on projects page

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,3 +6,21 @@ window.onload = function () {
         nav.classList.toggle('is-active');
     });
 }
+
+var request = new XMLHttpRequest();
+
+request.open('GET', ' https://api.github.com/users/davenfroberg/repos', true);
+
+request.onload = function() {
+    var datas = JSON.parse(this.response);
+    var descriptions = datas.map((data) => data.description);
+    descriptions.forEach(function(description) {
+        var ul = document.getElementById("projectList");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(description));
+        li.classList.add('project');
+        ul.appendChild(li);
+    });
+}
+
+request.send();
