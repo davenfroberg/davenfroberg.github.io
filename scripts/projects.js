e204f61d5e06aa95939e94676f80aa8dcca3f7d1
davenfroberg <davenfroberg@gmail.com>
2023-06-10 19:10:43 -0600

    Made progress on API readme endpoint

--- scripts/projects.js
+++ scripts/projects.js
@@ -7,20 +7,35 @@ window.onload = function () {
     });
 }
 
-var request = new XMLHttpRequest();
+var reposRequest = new XMLHttpRequest();
 
-request.open('GET', ' https://api.github.com/users/davenfroberg/repos', true);
+reposRequest.open('GET', 'https://api.github.com/users/davenfroberg/repos', true);
 
-request.onload = function() {
+reposRequest.onload = function() {
     var datas = JSON.parse(this.response);
-    var descriptions = datas.map((data) => data.description);
-    descriptions.forEach(function(description) {
+
+    datas.forEach(function(data) {
+        description = data.description;
         var ul = document.getElementById("projectList");
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(description));
         li.classList.add('project');
+        li.setAttribute('id', data.name)
         ul.appendChild(li);
+
+        var full_name = data.full_name;
+        var branch = data.default_branch;
+
+        var readMeRequest = new XMLHttpRequest();
+        readMeRequest.open('GET', `https://raw.githubusercontent.com/${full_name}/${branch}/README.md`, true);
+
+        readMeRequest.onload = function() {
+            var readme = this.response;
+            console.log(readme);
+        }
+        readMeRequest.send();
     });
 }
 
-request.send();
+reposRequest.send();
+
