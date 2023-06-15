ef64e2d209fa51ab447652383db0af13e636aa2e
Daven Froberg <96971299+davenfroberg@users.noreply.github.com>
2023-06-15 16:04:40 -0600

    Projects Page (#2)
    * Started work on projects page
    
    * Made progress on API readme endpoint
    
    * Added more readme changes
    
    * projects changes plus home page reformatting
    
    * Visual changes
    
    * Cleaned up CSS and projects page

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,3 +1,5 @@
+var username = 'davenfroberg';
+
 window.onload = function () {
     const menu_button = document.querySelector('.hamburger');
     const nav = document.querySelector('.nav');
@@ -6,3 +8,36 @@ window.onload = function () {
         nav.classList.toggle('is-active');
     });
 }
+
+fetch(`https://api.github.com/users/${username}/repos`).then((response) => response.json()).then(function(datas) {
+    datas.forEach(function(data) {
+        //skips profile readme repository
+        if (data.name == username)
+            return;
+
+        description = data.description;
+        var ul = document.getElementById("project-list");
+        var li = document.createElement("li");
+        var div = document.createElement("div");
+        li.appendChild(div);
+        div.appendChild(document.createTextNode(description));
+        div.classList.add('project');
+        div.setAttribute('id', data.name);
+        
+        ul.appendChild(li);
+
+        var full_name = data.full_name;
+        var branch = data.default_branch;
+        if (data.language != null) {
+            var language = document.createElement('div');
+            language.appendChild(document.createTextNode(data.language));
+            li.appendChild(language);
+            language.classList.add('language');
+        }
+
+        document.getElementById(data.name).addEventListener('click', function() {
+            window.open(data.html_url, '_blank');
+        }, false);
+    });
+});
+
