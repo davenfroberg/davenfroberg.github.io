1bc96c7a077d9ff0ae1c5e167ac463722838d1ef
davenfroberg <davenfroberg@gmail.com>
2024-12-26 00:24:07 -0700

    added skills

--- scripts/experience.js
+++ scripts/experience.js
@@ -6,3 +6,13 @@ window.onload = function () {
         nav.classList.toggle('is-active');
     });
 }
+
+function toggleSkills(nth) {
+    const quote = document.getElementsByClassName('quote').item(nth);
+    const skillsContent = document.getElementsByClassName('skillsContent').item(nth);
+    const clickMe = quote.querySelector('.clickMe');
+    clickMe.classList.add('hidden');
+    quote.classList.toggle('hidden');
+    skillsContent.classList.toggle('hidden');
+  }
+  
