6af13ab04f0c79b7d9c0ddcf04f80493e3615a17
davenfroberg <davenfroberg@gmail.com>
2026-05-01 22:07:15 -0700

    HUGE refactor of website

--- scripts/experience.js
+++
@@ -1,9 +0,0 @@
-function toggleSkills(nth) {
-    const quote = document.getElementsByClassName('quote').item(nth);
-    const skillsContent = document.getElementsByClassName('skillsContent').item(nth);
-    const clickMe = quote.querySelector('.clickMe');
-    // clickMe.classList.toggle('hidden');
-    quote.classList.toggle('hidden');
-    skillsContent.classList.toggle('hidden');
-  }
-  
